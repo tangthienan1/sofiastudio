bcd73f95ec9093fb6740d8ea5676c30541bbea8c
AndyTang <tangthienan9@gmail.com>
2023-07-22 22:26:09 +0700

    Add public folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { FC } from 'react';
+
+const Home: FC = () => {
+    return (
+        <>
+            <Head>
+            </Head>
+            <main >
+              thiennnnn
+            </main>
+        </>
+    );
+};
+
+export default Home
