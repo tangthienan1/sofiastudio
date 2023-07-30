32ea368f88e3dbfde9082084131b071440e91953
AndyTang <tangthienan9@gmail.com>
2023-07-30 17:05:21 +0700

    fix type HighlightNumberByText

--- src/components/HighlightNumberByText/HighlightNumberByText.tsx
+++ src/components/HighlightNumberByText/HighlightNumberByText.tsx
@@ -3,17 +3,15 @@ import { FC } from 'react';
 import { HighlightContentsType } from '@/data/aboutPageData';
 import AnimatedNumber from './AnimatedNumber';
 
-type HighlightNumberByTextType = HighlightContentsType & {
-    key?: string | number;
-};
-
-const HighlightNumberByText: FC<HighlightNumberByTextType> = ({ number, content, key }) => {
+const HighlightNumberByText: FC<HighlightContentsType> = ({ number, content }) => {
     return (
-        <div className="flex flex-col items-end justify-center" key={key}>
+        <div className="flex flex-col items-end justify-center">
             <span className="inline-block text-7xl font-bold">
                 <AnimatedNumber value={number} />+
             </span>
-            <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">{content}</h2>
+            <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
+                {content}
+            </h2>
         </div>
     );
 };
