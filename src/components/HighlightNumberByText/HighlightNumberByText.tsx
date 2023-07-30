a2403f0f4d911ac6dab898e82cb646a5b58e8022
AndyTang <tangthienan9@gmail.com>
2023-07-30 17:22:06 +0700

    responsive about page

--- src/components/HighlightNumberByText/HighlightNumberByText.tsx
+++ src/components/HighlightNumberByText/HighlightNumberByText.tsx
@@ -5,11 +5,14 @@ import AnimatedNumber from './AnimatedNumber';
 
 const HighlightNumberByText: FC<HighlightContentsType> = ({ number, content }) => {
     return (
-        <div className="flex flex-col items-end justify-center">
-            <span className="inline-block text-7xl font-bold">
+        <div className="flex flex-col items-end justify-center xl:items-center">
+            <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                 <AnimatedNumber value={number} />+
             </span>
-            <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
+            <h2
+                className="text-xl font-medium capitalize text-dark/75 dark:text-light/75
+            xl:text-center md:text-lg sm:text-base xs:text-sm"
+            >
                 {content}
             </h2>
         </div>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,19 +9,27 @@ import experiences from '@/components/PersonalBackground/experiences';
 import Skills from '@/components/Skills';
 import CustomHead from '@/components/CustomHead';
 
-import { aboutTitle, biographyContentsData, educations, highlightContentsData } from '@/data/aboutPageData';
+import {
+    aboutTitle,
+    biographyContentsData,
+    educations,
+    highlightContentsData,
+} from '@/data/aboutPageData';
 
 import profilePic from '../../public/images/profile/developer-pic-2.jpg';
 
 const About = () => {
     return (
         <>
-            <CustomHead title="About Page" content="any"/>
+            <CustomHead title="About Page" content="any" />
             <main className="flex w-full flex-col items-center justify-center dark:text-light">
                 <Layout>
-                    <AnimatedText text={aboutTitle} className="mb-16" />
-                    <div className="grid w-full grid-cols-8 gap-16">
-                        <div className="col-span-3 flex flex-col items-start justify-start">
+                    <AnimatedText
+                        text={aboutTitle}
+                        className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+                    />
+                    <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+                        <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
                             <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                                 Biography
                             </h2>
@@ -32,7 +40,10 @@ const About = () => {
                             ))}
                         </div>
 
-                        <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light">
+                        <div
+                            className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8
+                        dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8"
+                        >
                             <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
                             <Image
                                 src={profilePic}
@@ -43,7 +54,10 @@ const About = () => {
                             />
                         </div>
 
-                        <div className="col-span-2 flex flex-col items-end justify-between">
+                        <div
+                            className="col-span-2 flex flex-col items-end justify-between
+                        xl:col-span-8 xl:flex-row xl:items-center md:order-3"
+                        >
                             {highlightContentsData.map((highlightContent, index) => (
                                 <HighlightNumberByText {...highlightContent} key={index} />
                             ))}
