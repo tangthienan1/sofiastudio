c3b45ddc8e2ea661a121fa89dc955aca5e9728e2
AndyTang <tangthienan9@gmail.com>
2023-07-30 18:03:48 +0700

    update HireMe sm position + fix navbar size for mobile

--- src/components/NavBar/MobileNavbar.tsx
+++ src/components/NavBar/MobileNavbar.tsx
@@ -17,9 +17,9 @@ const MobileNavbar: FC<MobileNavbarType> = ({ toggleMode, onDarkModeClick, mode 
         <motion.div
             initial={{ scale: 0, opacity: 0, x: '-50%', y: '-50%' }}
             animate={{ scale: 1, opacity: 1 }}
-            className="min-w-[70vw] flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-            z-30 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md p-32  
-            "
+            className="min-w-[70vw] sm:min-w-[90vw] flex justify-between items-center flex-col
+            fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-32 bg-dark/90 dark:bg-light/75
+            rounded-lg z-50 backdrop-blur-md"
         >
             <nav className="flex items-center flex-col justify-center">
                 {navbarItems.pages.map((page, index) => (
