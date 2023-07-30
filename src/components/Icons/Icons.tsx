4e0dca87c34e3fd0c239102469e7650c0bfa9b3b
AndyTang <tangthienan9@gmail.com>
2023-07-30 20:22:30 +0700

    hot fix set in svg

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -139,7 +139,7 @@ export const SunIcon: FC<IconType> = ({ className, ...rest }) => (
                 d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z"
                 opacity="0"
             >
-                <set attributeName="opacity" begin="0.5s" to="1" />
+                {/* <set attributeName="opacity" begin="0.5s" to="1" /> */}
             </path>
         </g>
         <g fill="currentColor" fillOpacity="0">
@@ -229,7 +229,7 @@ export const SunIcon: FC<IconType> = ({ className, ...rest }) => (
             fill="currentColor"
             mask="url(#lineMdSunnyFilledLoopToMoonFilledLoopTransition1)"
         >
-            <set attributeName="opacity" begin="0.5s" to="0" />
+            {/* <set attributeName="opacity" begin="0.5s" to="0" /> */}
             <animate fill="freeze" attributeName="r" begin="0.1s" dur="0.4s" values="6;10" />
         </circle>
     </svg>
@@ -315,7 +315,7 @@ export const MoonIcon: FC<IconType> = ({ className, ...rest }) => (
             strokeWidth="2"
         >
             <path d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z" />
-            <set attributeName="opacity" begin="0.6s" to="0" />
+            {/* <set attributeName="opacity" begin="0.6s" to="0" /> */}
         </g>
         <mask id="lineMdMoonFilledToSunnyFilledLoopTransition0">
             <circle cx="12" cy="12" r="12" fill="#fff" />
@@ -338,7 +338,7 @@ export const MoonIcon: FC<IconType> = ({ className, ...rest }) => (
             mask="url(#lineMdMoonFilledToSunnyFilledLoopTransition0)"
             opacity="0"
         >
-            <set attributeName="opacity" begin="0.6s" to="1" />
+            {/* <set attributeName="opacity" begin="0.6s" to="1" /> */}
             <animate fill="freeze" attributeName="r" begin="0.6s" dur="0.4s" values="10;6" />
         </circle>
     </svg>
