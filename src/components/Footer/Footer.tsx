1fa0dc4ff7af4f0309fb07ece8c86d545ba6b17d
AndyTang <tangthienan9@gmail.com>
2023-07-31 12:39:37 +0700

    upload footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
 import Link from 'next/link';
-
-import Layout from '../Layout';
 
 const Footer = () => {
     return (
         <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:border-light dark:text-light sm:text-base">
-            <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
+            <div className="py-6 flex items-center justify-around lg:flex-col">
                 <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-                <div className="flex items-center lg:py-2">
+                <div className="flex items-center">
                     Build With <span className="text-primary text-2xl px-1 dark:text-primaryDark">&#9825;</span> by&nbsp;
                     <Link href="https://www.linkedin.com/in/tang-thien-an-0691a4195/" target='_blank' className="underline underline-offset-2">
                         ND
@@ -21,7 +18,7 @@ const Footer = () => {
                 >
                     Say Hello
                 </Link>
-            </Layout>
+            </div>
         </footer>
     );
 };
